950b77b516432fa5674a710a2bd959c53f965cb0
wzm-pl <896377290@qq.com>
2021-01-12 20:16:35 +0800

    zuul网关

---
+++ zuul/src/main/java/com/jk/ZuulApplication.java
@@ -0,0 +1,19 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableRedisHttpSession
+@EnableZuulProxy
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+}
